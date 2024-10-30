a68cdad7ca50a55855c7cc5bde8dc13570f3d880
youtao guo <coyorkdow@outlook.com>
2024-10-31 07:23:57 +0800

    WIP: scheduler

--- promise.hpp
+++ promise.hpp
@@ -8,21 +8,17 @@
 #include <queue>
 #include <thread>
 #include <type_traits>
+#include <utility>
 #include <vector>
 
 namespace coro {
 
 template <class Tp>
-  requires(!std::is_reference_v<Tp>)
-struct run_to_done_awaiter;
+struct async_awaiter;
+
+struct suspend_always : std::suspend_always {};
 
 class static_thread_pool;
-
-struct suspend_t : std::suspend_always {};
-
-namespace this_scheduler {
-inline suspend_t suspend;
-}  // namespace this_scheduler
 
 template <class Tp>
   requires(!std::is_reference_v<Tp>)
@@ -31,44 +27,55 @@ class task;
 enum class task_type {
   deferred,
   async,
-  async_runner,
 };
 
-class task_error : public std::logic_error {
- public:
-  const char* what() const noexcept override {
-    return std::logic_error::what();
-  }
-  explicit task_error(const std::string& msg)
-      : std::logic_error("coro::task_error: " + msg) {}
-};
+namespace this_scheduler {
+inline suspend_always yield;
+}  // namespace this_scheduler
 
 namespace details_ {
+struct coro_context {
+  std::queue<std::coroutine_handle<>> ctx_wait_que_;
+  std::mutex ctx_mu_;
+};
+
 template <class Tp>
 class task_base;
 
 template <class Tp>
-struct promise_base : std::promise<Tp> {
+struct final_suspender;
+
+template <class Tp>
+struct promise_base : std::promise<Tp>, protected coro_context {
   task<Tp> get_return_object() noexcept;
 
   std::suspend_always initial_suspend() const noexcept { return {}; }
-  std::suspend_always final_suspend() const noexcept { return {}; }
+
+  inline final_suspender<Tp> final_suspend() noexcept;
 
   template <class Awaiter>
   void await_transform(Awaiter) = delete;
 
-  suspend_t await_transform(suspend_t) { return this_scheduler::suspend; }
+  suspend_always await_transform(suspend_always) { return suspend_always{}; }
 
-  template <class R>
-  inline run_to_done_awaiter<R> await_transform(task<R>) noexcept;
+  template <class Up>
+  inline async_awaiter<Up> await_transform(task<Up>) noexcept;
 
   void unhandled_exception() noexcept {
     this->set_exception(std::current_exception());
   }
 
-  promise_base() : fu_(this->get_future()) {}
+ protected:
+  template <class Up>
+  friend struct promise_base;
 
-  std::future<Tp> fu_;
+  promise_base() : scheduler_(nullptr), suspend_on_final_(true) {}
+
+  static_thread_pool* scheduler_;
+  bool suspend_on_final_;
+
+  template <class Up>
+  friend struct final_suspender;
 };
 }  // namespace details_
 
@@ -76,21 +83,25 @@ template <class Tp>
   requires(!std::is_reference_v<Tp>)
 class promise : public details_::promise_base<Tp> {
  public:
-  void return_value(const Tp& value) noexcept(
-      std::is_nothrow_copy_constructible_v<Tp>) {
-    this->set_value(value);
-  }
-
-  void return_value(Tp&& value) noexcept(
-      std::is_nothrow_move_constructible_v<Tp>) {
-    this->set_value(std::move(value));
+  template <class Up>
+  void return_value(Up&& value)
+    requires(std::is_same_v<Tp, Up> || std::is_same_v<const Tp&, Up>)
+  {
+    this->set_value(std::forward<Up>(value));
   }
 
  private:
-  template <class Rp>
+  template <class Up>
   friend class details_::task_base;
 
-  using details_::promise_base<Tp>::fu_;
+  template <class Up>
+  friend struct async_awaiter;
+
+  template <class Up>
+    requires(!std::is_reference_v<Up>)
+  friend struct synced_waker;
+
+  friend class static_thread_pool;
 };
 
 template <>
@@ -99,23 +110,48 @@ class promise<void> : public details_::promise_base<void> {
   void return_void() noexcept { this->set_value(); }
 
  private:
-  template <class Rp>
+  template <class Up>
   friend class details_::task_base;
 
-  using details_::promise_base<void>::fu_;
+  template <class Up>
+  friend struct async_awaiter;
+
+  friend class static_thread_pool;
 };
 
-namespace details_ {
+template <class>
+struct is_task : std::false_type {};
 
+template <class Tp>
+struct is_task<task<Tp>> : std::true_type {};
+
+template <class Tp>
+inline constexpr bool is_task_v = is_task<Tp>::value;
+
+template <class>
+struct is_promise : std::false_type {};
+
+template <class Tp>
+struct is_promise<promise<Tp>> : std::true_type {};
+
+template <class Tp>
+inline constexpr bool is_promise_v = is_promise<Tp>::value;
+
+namespace details_ {
 template <class Tp>
 class task_base {
  public:
-  bool valid() const noexcept { return handle_ && get_future().valid(); }
+  bool valid() const noexcept { return fu_.valid(); }
+
+  task_type type() const noexcept { return typ_; }
+
   void wait() const {
     if (typ_ == task_type::deferred) {
-      run_to_done();
+      while (!done()) {
+        resume();
+      }
     } else {
-      get_future().wait();
+      fu_.wait();
     }
   }
 
@@ -125,44 +161,64 @@ class task_base {
     if (typ_ == task_type::deferred) {
       return std::future_status::deferred;
     }
-    return get_future().wait_for(rel);
+    return fu_.wait_for(rel);
   }
 
-  Tp get() const {
-    if (typ_ == task_type::async_runner) {
-      throw task_error("get value from an async runner task is not allowed");
-    }
+  Tp get() {
     wait();
     if constexpr (std::is_same_v<Tp, void>) {
-      get_future().get();
-      handle_.destroy();
+      fu_.get();
+      // Handle of the non-async task should be destroyed here.
+      if (typ_ == task_type::deferred) {
+        handle_.destroy();
+      }
     } else {
-      Tp res = get_future().get();
-      handle_.destroy();
+      Tp res = fu_.get();
+      if (typ_ == task_type::deferred) {
+        handle_.destroy();
+      }
       return res;
     }
   }
 
   void resume() const { handle_.resume(); }
+
   bool done() const noexcept { return handle_.done(); }
 
- protected:
-  std::future<Tp>& get_future() const { return handle_.promise().fu_; }
+  void destroy() const { handle_.destroy(); }
 
-  void run_to_done() const {
-    while (!done()) {
-      this->handle_.resume();
+  std::coroutine_handle<promise<Tp>> release_coroutine_handle() noexcept {
+    auto h = handle_;
+    handle_ = nullptr;
+    typ_ = task_type::async;
+    return h;
+  }
+
+ protected:
+  task_base(std::coroutine_handle<promise<Tp>> h = nullptr,
+            task_type t = task_type::deferred) noexcept
+      : handle_(h), typ_(t) {
+    if (h) {
+      fu_ = h.promise().get_future();
     }
   }
 
-  task_base(std::coroutine_handle<promise<Tp>> h = nullptr,
-            task_type t = task_type::deferred) noexcept
-      : handle_(h), typ_(t) {}
+  task_base(task_base&& rhs) noexcept { *this = std::move(rhs); }
+
+  task_base& operator=(task_base&& rhs) noexcept {
+    handle_ = rhs.handle_;
+    fu_ = std::move(rhs.fu_);
+    typ_ = rhs.typ_;
+    rhs.handle_ = nullptr;
+    return *this;
+  }
+
+  promise<Tp>& get_promise() const { return handle_.promise(); }
 
   std::coroutine_handle<promise<Tp>> handle_;
+  std::future<Tp> fu_;
   task_type typ_;
 };
-
 }  // namespace details_
 
 template <class Tp>
@@ -172,13 +228,8 @@ class task : public details_::task_base<Tp> {
   using promise_type = promise<Tp>;
 
   task() noexcept {}
-  task(task&& rhs) noexcept { *this = std::move(rhs); }
-  task& operator=(task&& rhs) noexcept {
-    this->handle_ = rhs.handle_;
-    this->typ_ = rhs.typ_;
-    rhs.handle_ = nullptr;
-    return *this;
-  }
+  task(task&& rhs) noexcept = default;
+  task& operator=(task&& rhs) noexcept = default;
 
  protected:
   task(std::coroutine_handle<promise_type> h, task_type t)
@@ -187,40 +238,8 @@ class task : public details_::task_base<Tp> {
   template <class Rp>
   friend struct details_::promise_base;
 
-  template <class Rp>
-  friend task<Rp> make_async_task(task<Rp>&) noexcept;
+  friend class static_thread_pool;
 };
-
-template <>
-class task<void> : public details_::task_base<void> {
- public:
-  using promise_type = promise<void>;
-
-  task() noexcept {}
-  task(task&& rhs) noexcept { *this = std::move(rhs); }
-  task& operator=(task&& rhs) noexcept {
-    this->handle_ = rhs.handle_;
-    this->typ_ = rhs.typ_;
-    rhs.handle_ = nullptr;
-    return *this;
-  }
-
- protected:
-  task(std::coroutine_handle<promise_type> h, task_type t)
-      : details_::task_base<void>(h, t) {}
-
-  template <class Rp>
-  friend struct details_::promise_base;
-
-  template <class Rp>
-  friend task<Rp> make_async_task(task<Rp>&) noexcept;
-};
-
-template <class Tp>
-task<Tp> make_async_task(task<Tp>& t) noexcept {
-  t.typ_ = task_type::async_runner;
-  return task<Tp>(t.handle_, task_type::async);
-}
 
 template <class Tp>
 task<Tp> details_::promise_base<Tp>::get_return_object() noexcept {
@@ -229,59 +248,146 @@ task<Tp> details_::promise_base<Tp>::get_return_object() noexcept {
                   task_type::deferred);
 }
 
-template <class Tp>
-template <class R>
-inline run_to_done_awaiter<R> details_::promise_base<Tp>::await_transform(
-    task<R> t) noexcept {
-  return run_to_done_awaiter<R>{std::move(t)};
-}
-
-template <class Tp>
-  requires(!std::is_reference_v<Tp>)
-struct run_to_done_awaiter : public coro::task<Tp> {
-  bool await_ready() const noexcept {
-    using namespace std::chrono_literals;
-    return this->wait_for(0s) != std::future_status::timeout;
-  }
-
-  bool await_suspend(std::coroutine_handle<>) const {
-    this->run_to_done();
-    return false;
-  }
-
-  Tp await_resume() { return this->get(); }
-};
-
 class static_thread_pool {
  public:
-  explicit static_thread_pool(size_t n) : exit_(false) {}
+  explicit static_thread_pool(size_t n) : exit_(false) {
+    for (size_t i = 0; i < n; i++) {
+      ths_.push_back(std::thread(&static_thread_pool::worker_routine, this));
+    }
+  }
 
-  template <class Tp>
-  task<Tp> emplace(task<Tp> t) {
+  ~static_thread_pool() {
     {
       std::unique_lock l(mu_);
-      // que_.emplace(Args &&args...)
+      exit_ = true;
     }
+    con_.notify_all();
+    for (auto& th : ths_) {
+      th.join();
+    }
+  }
+
+  template <class Tp>
+  task<Tp> schedule(task<Tp>&& t) {
+    schedule(t.release_coroutine_handle());
+    return t;
+  }
+
+  template <class Tp>
+  task<Tp>& schedule(task<Tp>& t) {
+    schedule(t.release_coroutine_handle());
+    return t;
   }
 
  private:
+  template <class Tp>
+  friend struct details_::final_suspender;
+
+  template <class Tp>
+  void schedule(std::coroutine_handle<Tp> handle) {
+    if constexpr (is_promise_v<Tp>) {
+      handle.promise().suspend_on_final_ = false;
+      handle.promise().scheduler_ = this;
+    }
+    {
+      std::unique_lock l(mu_);
+      que_.push(handle);
+    }
+    con_.notify_one();
+  }
+
   void worker_routine() {
     while (true) {
-      task<void> t;
+      std::coroutine_handle<> t;
       {
         std::unique_lock l(mu_);
         con_.wait(l, [&] { return exit_ || !que_.empty(); });
+        if (que_.empty() && exit_) {
+          break;
+        }
         t = std::move(que_.front());
         que_.pop();
       }
+      t.resume();
     }
   }
 
-  std::queue<task<void>> que_;
+  std::queue<std::coroutine_handle<>> que_;
   std::mutex mu_;
   std::condition_variable con_;
   std::vector<std::thread> ths_;
   bool exit_;
 };
+
+template <class Tp>
+struct async_awaiter : public coro::task<Tp> {
+  bool await_ready() const noexcept {
+    using namespace std::chrono_literals;
+    return this->wait_for(0s) == std::future_status::ready;
+  }
+
+  void await_suspend(std::coroutine_handle<> h) {
+    assert(this->typ_ == task_type::async);
+    promise<Tp>& promise = this->get_promise();
+    {
+      std::unique_lock l(promise.ctx_mu_);
+      promise.ctx_wait_que_.push(h);
+    }
+    if (promise.scheduler_) {
+      promise.scheduler_->schedule(static_cast<task<Tp>&>(*this));
+    } else {
+      while (!this->done()) {
+        this->resume();
+      }
+      this->destroy();
+    }
+  }
+
+  Tp await_resume() { return this->get(); }
+};
+
+template <class Tp>
+struct details_::final_suspender {
+  constexpr bool await_ready() const noexcept { return false; }
+
+  bool await_suspend(std::coroutine_handle<>) const noexcept {
+    std::unique_lock l(self->ctx_mu_);
+    auto wait_que = std::move(self->ctx_wait_que_);
+    auto sch = self->scheduler_;
+    bool suspend_on_final = self->suspend_on_final_;
+    // Current coroutine might be destroyed by the coroutines resumed in this
+    // function. Promise object (which is stored in coroutine frame) is not
+    // allowed to use.
+    while (!wait_que.empty()) {
+      auto handle = wait_que.front();
+      wait_que.pop();
+      if (sch) {
+        sch->schedule(handle);
+      } else {
+        handle.resume();
+      }
+    }
+    return suspend_on_final;
+  }
+
+  constexpr void await_resume() const noexcept {}
+
+  promise_base<Tp>* self;
+};
+
+template <class Tp>
+template <class Up>
+inline async_awaiter<Up> details_::promise_base<Tp>::await_transform(
+    task<Up> t) noexcept {
+  t.get_promise().scheduler_ = scheduler_;
+  t.typ_ = task_type::async;
+  return async_awaiter<Up>{std::move(t)};
+}
+
+template <class Tp>
+inline details_::final_suspender<Tp>
+details_::promise_base<Tp>::final_suspend() noexcept {
+  return {this};
+}
 
 }  // namespace coro
--- test.cc
+++ test.cc
@@ -5,14 +5,14 @@
 
 #include "promise.hpp"
 
-coro::task<void> do_nothing() {
-  std::cout << "I don't want to do anything!\n";
+coro::task<void> print_tid() {
+  std::cout << "thread id is " << std::this_thread::get_id() << '\n';
   co_return;
 }
 
 coro::task<int> fibonacci(int n) {
   if (n == 0 || n == 1) {
-    co_await do_nothing();
+    co_await print_tid();
     co_return n;
   }
   co_return co_await fibonacci(n - 1) + co_await fibonacci(n - 2);
@@ -24,27 +24,31 @@ coro::task<std::string> foo(std::string v) {
 }
 
 int main() {
-  auto fib = fibonacci(10);
+  auto fib = fibonacci(1);
   fib.wait();
   std::cout << fib.get() << '\n';
 
   auto f = foo("abc");
-  auto r = coro::make_async_task(f);
 
-  std::jthread th([r = std::move(r)] {
-    std::cout << r.get();
-    std::cout << "\nsub thread exit\n";
-  });
   auto b = [](auto f) -> coro::task<void> {
     using namespace std::chrono_literals;
     std::this_thread::sleep_for(1s);
     while (!f.done()) {
       f.resume();
-      co_await coro::this_scheduler::suspend;
+      co_await coro::this_scheduler::yield;
     }
-    f.wait();
+    f.destroy();
     co_return;
-  }(std::move(f));
+  }(f.release_coroutine_handle());
+
+  std::jthread th([f = std::move(f)]() mutable {
+    std::cout << f.get();
+    std::cout << "\nsub thread exit\n";
+  });
+
   b.get();
   static_assert(!std::is_copy_constructible_v<decltype(f)>);
+
+  coro::static_thread_pool pool(3);
+  std::cout << pool.schedule(fibonacci(10)).get() << std::endl;
 }
